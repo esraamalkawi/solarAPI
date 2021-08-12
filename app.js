57346d4c0cf0c580137de9c29a767964bee74433
esraamalkawi <esraamalkawi462@gmail.com>
2021-08-12 06:46:17 -0700

    spaceImg done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const authRoutes = require("./routes/authRoute");
 const userRoutes = require("./routes/userRoute");
-
+const spaceImgRoute = require("./routes/spaceImgRoute");
 const itemRoutes = require("./routes/itemRoute");
 
 const passport = require("passport");
@@ -22,6 +22,7 @@ passport.use(jwtStrategy);
 app.use(authRoutes);
 app.use(userRoutes);
 app.use("/items", itemRoutes);
+app.use("/images", spaceImgRoute);
 app.use("/media", express.static(path.join(__dirname, "media")));
 
 app.use((err, req, res, next) => {
