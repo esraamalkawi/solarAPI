14c88a1095647782514072bd8da2b7c502af5a8b
esraamalkawi <esraamalkawi462@gmail.com>
2021-08-17 08:37:36 -0700

    buying item is done

--- db/models/UserStore.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const UserStore = sequelize.define("UserStore", {});
-  UserStore.associate = (models) => {
-    models.User.hasMany(UserStore, { foreignKey: "userId" });
-    UserStore.belongsTo(models.User, { foreignKey: "userId" });
-  };
-  return UserStore;
-};
